c51abc8078f5b276148a19bb5998f19ea84a59e7
Andrew Durrant <andrew.durrant75@gmail.com>
2023-04-04 18:11:55 -0600

    Initial commit with Firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUfw0QTqLFqOu9Nd0apnLnOkydpU6dBLw",
+  authDomain: "fir-frontend-137d1.firebaseapp.com",
+  projectId: "fir-frontend-137d1",
+  storageBucket: "fir-frontend-137d1.appspot.com",
+  messagingSenderId: "642143690818",
+  appId: "1:642143690818:web:75b0796898399b9e0ecddb",
+  measurementId: "G-EFNH1Y2H1K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
